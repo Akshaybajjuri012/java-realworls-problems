4500d4d355dd879c5d8867747dc2fc620efb4f1b
BAJJURIAKSHAY <bajjuriakshay33@gmail.com>
2025-08-07 10:07:21 +0530

    Initial commit

---
+++ WiproEurekaDemo/src/test/java/com/wipro/eurekaexample/WiproEurekaDemo/WiproEurekaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wipro.eurekaexample.WiproEurekaDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiproEurekaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
